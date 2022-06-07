fbe664fde0d7f10e0ebab86adf1135cee72ffd9c
Sean Daly <sean@insightdesigns.com>
2022-06-07 13:13:46 -0600

    remove debugging

--- wiktionaryparser/core.py
+++ wiktionaryparser/core.py
@@ -159,8 +159,6 @@ class WiktionaryParser(object):
                 super_tag.clear()
             for list_element in list_tag.find_all('li'):
                 for audio_tag in list_element.find_all('td', {'class': 'audiometa'}):
-                    audio_links.append(audio_tag)
-                    audio_links.append("      seperator     ")
                     audio_links.append(audio_tag.find('a')['href'])
                     audio_tag.extract()
                 for nested_list_element in list_element.find_all('ul'):
